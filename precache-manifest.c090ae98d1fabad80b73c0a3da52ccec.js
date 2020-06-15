9a61791bdd5c186dc9ad7f7ea02280bdf6c9fec1
themoonofendor <evoskolyey@gmail.com>
2020-06-15 17:25:20 -0400

    Updates

---
+++ precache-manifest.c090ae98d1fabad80b73c0a3da52ccec.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8012da564a2aca9f0e95ba0eb4ab82f",
+    "url": "/kanban1/index.html"
+  },
+  {
+    "revision": "32493eddff606ddbe29c",
+    "url": "/kanban1/static/css/2.e7c4c567.chunk.css"
+  },
+  {
+    "revision": "e0eb6bb3bfdb91d1df52",
+    "url": "/kanban1/static/css/main.21fa802b.chunk.css"
+  },
+  {
+    "revision": "32493eddff606ddbe29c",
+    "url": "/kanban1/static/js/2.e114783d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/kanban1/static/js/2.e114783d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0eb6bb3bfdb91d1df52",
+    "url": "/kanban1/static/js/main.8ccc262c.chunk.js"
+  },
+  {
+    "revision": "07fd7ddef3441cdfc1eb",
+    "url": "/kanban1/static/js/runtime-main.721114f5.js"
+  },
+  {
+    "revision": "11907afecfead1307c1ae81efcc65f57",
+    "url": "/kanban1/static/media/blocker.11907afe.svg"
+  },
+  {
+    "revision": "8880f1fb16be8b2a3b2c7ddc516a7c27",
+    "url": "/kanban1/static/media/critical.8880f1fb.svg"
+  },
+  {
+    "revision": "d7322dc23149f82569df4e87056c8aa8",
+    "url": "/kanban1/static/media/help.d7322dc2.svg"
+  },
+  {
+    "revision": "0f285b32f5eb17b98eb222e54f13f191",
+    "url": "/kanban1/static/media/major.0f285b32.svg"
+  },
+  {
+    "revision": "b243f90c28ef8bb55e66b09d656fdb87",
+    "url": "/kanban1/static/media/menu.b243f90c.svg"
+  },
+  {
+    "revision": "cdf6fc5175fc6de04961fd5a906c912a",
+    "url": "/kanban1/static/media/minor.cdf6fc51.svg"
+  },
+  {
+    "revision": "ddbb469398a2c447f84dd25b1477da13",
+    "url": "/kanban1/static/media/pencil.ddbb4693.svg"
+  },
+  {
+    "revision": "7b65c534921181272e89017863bab1a9",
+    "url": "/kanban1/static/media/plus.7b65c534.svg"
+  },
+  {
+    "revision": "49b3a28c21a218a4656b7b07a46c6916",
+    "url": "/kanban1/static/media/plusWhite.49b3a28c.svg"
+  },
+  {
+    "revision": "3cc612a20d4afc4479e434950a5ad4e8",
+    "url": "/kanban1/static/media/rhombus.3cc612a2.svg"
+  },
+  {
+    "revision": "2e0925c0ad579698b047358128d7bff8",
+    "url": "/kanban1/static/media/search.2e0925c0.svg"
+  },
+  {
+    "revision": "d98b87f01fe73662c1120d97ceaf0906",
+    "url": "/kanban1/static/media/trash.d98b87f0.svg"
+  },
+  {
+    "revision": "163f67f5d87b05b9b2f309034d6d360d",
+    "url": "/kanban1/static/media/trivial.163f67f5.svg"
+  },
+  {
+    "revision": "897168966da272a0f9eda1810ed91d5b",
+    "url": "/kanban1/static/media/user.89716896.svg"
+  }
+]);
