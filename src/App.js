a17bb6eb9d499f49d8f1677546be8046b9962903
eugene <eugene.voskoley@advance-auto.com>
2020-06-15 15:19:56 -0400

    rearranged components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {defaultTasks} from "./utils/Constants";
+import Board from "./components/Board";
+import {removeTaskAndReorderTasks} from "./utils/TaskUtil";
+import TopBar from "./components/TopBar";
+import {toast, ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import PrimaryLeftSidebar from "./components/PrimaryLeftSidebar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [tasks, setTasks] = useState(defaultTasks);
+
+    const updateTasks = (updatedTasks) => {
+        setTasks(updatedTasks);
+    };
+
+    const removeTask = (taskToBeRemoved) => {
+        const newTasks = removeTaskAndReorderTasks(tasks, taskToBeRemoved);
+        setTasks(newTasks);
+        toast.success('Successfully Deleted!');
+    };
+
+    const createTask = (task) => {
+        const newTasks = tasks.map(el => {
+            if (el.status === task.status) {
+                el.index = el.index + 1;
+            }
+            return el;
+        });
+        newTasks.push(task);
+        setTasks(newTasks);
+        toast.success('Successfully Created!');
+    };
+
+    const editTask = (editedTask) => {
+        const newTasks = [...tasks].map(el => {
+            if (el.id === editedTask.id) {
+                el.title = editedTask.title;
+                el.project = editedTask.project;
+                el.priority = editedTask.priority;
+            }
+            return el;
+        });
+        console.log(newTasks);
+        setTasks(newTasks);
+        toast.success('Successfully Saved!');
+    };
+
+    return (
+        <>
+            <ToastContainer
+                position="top-right"
+                style={{textAlign: 'center'}}
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss
+                draggable={true}
+                pauseOnHover={false}/>
+
+            <PrimaryLeftSidebar/>
+            <div className="col-3-w">
+                <TopBar createTask={createTask}/>
+                <Board tasks={tasks} updateTasks={updateTasks} removeTask={removeTask} editTask={editTask}/>
+            </div>
+        </>
+    );
 }
 
 export default App;
